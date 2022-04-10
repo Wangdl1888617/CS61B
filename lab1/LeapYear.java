c3e693b2e7f3383177f90c3225ff4784405efb48
wang dali <Wangdl1888617@163.com>
2022-04-10 09:44:23 +0800

    completed first part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 <<<<<<< HEAD
-/** Class that determines whether or not a year is a leap year.**
+/** Class that determines whether or not a year is a leap year.*
 <<<<<<< HEAD
  *  @author Dali Wang *
 =======
